ff60e72a029db73a63f75f18f397ed0e1805a715
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-09-29 06:40:44 +0200

    Ssimple form and validation

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-form',
+  selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
 
+  log(x){
+    console.log(x);
+    
+  }
   constructor() { }
 
   ngOnInit() {
